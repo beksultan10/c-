8e507e152903272b717c15d36ee9076fb8255e1f
beksultan10 <94545033+beksultan10@users.noreply.github.com>
2022-03-18 18:52:34 +0600

    Create dz24.03.cpp

---
+++ dz24.03.cpp
@@ -0,0 +1,30 @@
+//void fun( int a) 
+//{
+//	cout << "100 ball system vvod :" << endl;
+//	int vvod{ 0 };
+//	cin >> vvod;
+//	
+//	if (vvod > 90 && vvod <= 100) {
+//		cout << " 5" << endl;
+//	}
+//
+//	if (vvod > 70 && vvod < 89) {
+//		cout << "4" << endl;
+//	}
+//	if (vvod > 50 && vvod < 69 ) {
+//		cout << "3" << endl;
+//	}
+//	if (vvod > 1 && vvod < 49) {
+//		cout << "2" << endl;
+//	}
+//	else {
+//		cout << "eshe raz" << endl;
+//	}
+//
+//
+//}
+//
+//int main() {
+//	int s{ 0 };
+//	fun(s);
+//}
